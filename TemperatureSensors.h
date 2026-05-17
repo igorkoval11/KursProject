4b7783369ae889b6aba5dee1bdda63271f4cb342
Gurin Artemiy <Gurin_AA_25@student.grsu.by>
2026-05-17 13:51:05 +0300

    feat:add TemperatureSensors

---
+++ TemperatureSensors.h
@@ -0,0 +1,19 @@
+//
+// Created by Artemiy on 17.05.26.
+//
+
+
+#ifndef KURSPROJECT_TEMPERATURESENSORS_H
+#define KURSPROJECT_TEMPERATURESENSORS_H
+#include "IoTDevice.h"
+
+
+class TemperatureSensor : public IoTDevice {
+    static constexpr int BASE_POWER = 10;
+
+public:
+    explicit TemperatureSensor(std::string name)
+        : IoTDevice(std::move(name), BASE_POWER, 2) {}
+    void update() override;
+};
+#endif //KURSPROJECT_TEMPERATURESENSORS_H
