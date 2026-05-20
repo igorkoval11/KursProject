f7d6bb7b108372e63a094538c1492310cd86c0b0
Ковалевский Игорь Александрович <igorkovalevskij000@gmail.com>
2026-05-20 12:13:26 +0300

    Датчик температуры отдает температуру, которая влияет на все устройства (диапазон рабочий + перепады температуры изменяют потребление)

--- IoTDevice.h
+++ IoTDevice.h
@@ -4,16 +4,19 @@
 #ifndef KURSPROJECT_IOTDEVICE_H
 #define KURSPROJECT_IOTDEVICE_H
 #include <string>
+#include <cmath>
+
 class IoTDevice{
 protected:
     bool is_on = true;
+    int base_power;
     int power_consumption;
     int energy_consumed = 0;
     std::string name;
     int priority; 
 public:
     IoTDevice(std::string name, int power, int priority = 3)
-        : name(name), power_consumption(power), priority(priority){}
+        : name(name), base_power(power), power_consumption(power), priority(priority){}
     virtual ~IoTDevice(){}
     virtual void update()=0;
     virtual void turnOff(){
@@ -21,6 +24,9 @@ public:
     }
     virtual bool isOn() const{
         return is_on;
+    }
+    virtual bool canBeTurnedOff() const {
+        return true;
     }
     int getEnergy() const{
         return energy_consumed;
@@ -31,8 +37,14 @@ public:
     std::string getName() const{
         return name;
     }
+    static double& getCurrentTemperature() {
+        static double temp = 20.0;
+        return temp;
+    }
     void consumeEnergy() {
         if (is_on) {
+            double temp = getCurrentTemperature();
+            power_consumption = static_cast<int>(base_power * (1.0 + std::abs(temp - 20.0) * 0.012));
             energy_consumed += power_consumption;
         }
     }
--- TemperatureSensors.h
+++ TemperatureSensors.h
@@ -15,5 +15,8 @@ public:
     explicit TemperatureSensor(std::string name)
         : IoTDevice(std::move(name), BASE_POWER, 2) {}
     void update() override;
+    void turnOff() override;
+    bool isOn() const override;
+    bool canBeTurnedOff() const override;
 };
 #endif //KURSPROJECT_TEMPERATURESENSORS_H
